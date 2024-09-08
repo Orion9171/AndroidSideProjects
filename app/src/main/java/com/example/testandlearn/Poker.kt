2830a746fddc42941e4dc367e702f6984243b5c5
Hannah_Chang <zhangbinghan47@gmail.com>
2024-09-09 01:54:43 +0800

    Poker completed

--- app/src/main/java/com/example/testandlearn/Poker.kt
+++ app/src/main/java/com/example/testandlearn/Poker.kt
@@ -1,5 +1,19 @@
 package com.example.testandlearn
 
-fun main(){
+import androidx.compose.material3.CircularProgressIndicator
 
+fun main(){
+    val deck = mutableListOf<Int>()
+    for(i in 0 until 52){
+        deck.add(i)
+        if(i%13 == 0 && i!=0) println()
+        val a = i/13
+        var type = when(a){
+            1 -> "C"
+            2 -> "D"
+            3 -> "H"
+            else -> "S"
+        }
+        print("${(i%13)+1}$type")
+    }
 }
